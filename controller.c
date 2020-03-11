c4caf67bb7688c50f8591858b2b35965d1cb577c
rosalg <rosalg@stanford.edu>
2020-03-11 14:45:46 -0700

    Finished basic controller driver

--- controller.c
+++ controller.c
@@ -7,51 +7,93 @@
 #include "ringbuffer.h"
 #include "controller.h"
 
-static unsigned int IN;
-static unsigned int OUT;
+static unsigned int DATA;
+static unsigned int bytes[64];
 
-/*
-void send_bit(int bit) {
-    int val = 0;
-    if (bit) {
-        gpio_write(OUT, 0);
-        gpio_write(OUT, 1);
-        timer_delay_us(2);
-    } else {
-        gpio_write(OUT, 0);
-        timer_delay_us(2);
-        gpio_write(OUT, 1);
-    }
+void controller_write() {
+    gpio_set_output(DATA);
+    controller_asm_write();
+    gpio_set_input(DATA);
+ 
 }
-*/
 
-/*
-void send_start_word() {
-    // send 0100 0000 0000 0011 0000 0000
-    send_bit(0);
-    send_bit(1);
-    for (int i = 0; i < 12; i++) {
-        send_bit(0);
+void controller_read() {
+    int cycle = 0;
+    int low_time = 0;
+    while (cycle < 64) {
+        low_time = 0;
+        while (gpio_read(DATA) == 0) low_time++;
+        if (low_time >= 2) {
+            bytes[cycle] = 0;
+        } else {
+            bytes[cycle] = 1;
+        }
+        while (gpio_read(DATA) == 1);
+        cycle++;
     }
-    for (int i = 0; i < 2; i++) {
-        send_bit(1);
-    }
-    for (int i = 0; i < 6; i++) {
-        send_bit(0);
-    }
-    send_bit(1);
-    send_bit(0);
-    // Terminate the bit stream with a 1
-    send_bit(1);
-}*/
 
-void controller_init(unsigned int out_gpio, unsigned int in_gpio) {
-    OUT = out_gpio;
-    IN = in_gpio;
-    gpio_set_output(OUT);
-    gpio_set_input(IN);
-    //printf("Entering asm");
-    controller_start_asm();
-    printf("\n%d\n", gpio_read(20));
+}
+
+void controller_get_inputs() {
+    printf("\n\n\nStart Value: %d\n", bytes[CONTROLLER_START]);
+    printf("Y Value: %d\n", bytes[CONTROLLER_Y]);
+    printf("X Value: %d\n", bytes[CONTROLLER_X]);
+    printf("B Value: %d\n", bytes[CONTROLLER_B]);
+    printf("A Value: %d\n", bytes[CONTROLLER_A]);
+    printf("L Trigger: %d\n", bytes[CONTROLLER_L]);
+    printf("R Trigger: %d\n", bytes[CONTROLLER_R]);
+    printf("Z Value: %d\n", bytes[CONTROLLER_Z]);
+    printf("D-Pad Up: %d\n", bytes[CONTROLLER_D_UP]);
+    printf("D-Pad Down: %d\n", bytes[CONTROLLER_D_DOWN]);
+    printf("D-Pad Right: %d\n", bytes[CONTROLLER_D_RIGHT]);
+    printf("D-Pad Left: %d\n", bytes[CONTROLLER_D_LEFT]);
+    
+    int val = 0;
+    for (int i = 7; i >= 0; i--) {
+        val += (bytes[CONTROLLER_JOYSTICK_X_SB + i] << i);
+    }
+    printf("Joystick X Value: %d\n", val);
+    
+    val = 0;
+    for (int i = 7; i >= 0; i--) {
+        val += (bytes[CONTROLLER_JOYSTICK_Y_SB + i] << i);
+    }
+    printf("Joystick Y Value: %d\n", val);
+    
+    val = 0;
+    for (int i = 7; i >= 0; i--) {
+        val += (bytes[CONTROLLER_CSTICK_X_SB + i] << i);
+    }
+    printf("C-Stick X Value: %d\n", val);
+    
+    val = 0;
+    for (int i = 7; i >= 0; i--) {
+        val += (bytes[CONTROLLER_CSTICK_Y_SB + i] << i);
+    }
+    printf("C-Stick Y Value: %d\n", val);
+
+    val = 0;
+    for (int i = 7; i >= 0; i--) {
+        val += (bytes[CONTROLLER_LEFT_TRIGGER_SB + i] << i);
+    }
+    printf("L Trigger Value: %d\n", val);
+
+    val = 0;
+    for (int i = 7; i >= 0; i--) {
+        val += (bytes[CONTROLLER_RIGHT_TRIGGER_SB + i] << i);
+    }
+    printf("R Trigger Value: %d\n", val);
+
+ 
+}
+
+void controller_init(unsigned int data_gpio) {
+    DATA = data_gpio;
+    while (1) {
+        controller_write();
+        while (gpio_read(DATA));
+        controller_read();
+        controller_get_inputs();
+    }
 }
 
--- controller.h
+++ controller.h
@@ -1,12 +1,53 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-//void send_bit(int bit);
+typedef enum {
+    // Empty bit
+    // Empty bit
+    // Empty bit
+    CONTROLLER_START = 3,
+    CONTROLLER_Y = 4,
+    CONTROLLER_X = 5,
+    CONTROLLER_B = 6,
+    CONTROLLER_A = 7,
+    // Empty bit
+    CONTROLLER_L = 9,
+    CONTROLLER_R = 10,
+    CONTROLLER_Z = 11,
+    CONTROLLER_D_UP = 12,
+    CONTROLLER_D_DOWN = 13,
+    CONTROLLER_D_RIGHT = 14,
+    CONTROLLER_D_LEFT = 15,
+    // Joystick X values
+    //
+    // Bits 16-23 tell the extent of the X value for the JOYSTICK.
+    CONTROLLER_JOYSTICK_X_SB = 16,
+    // Joystick Y values
+    //
+    // Bits 24-31 tell the extent of the Y value for the JOYSTICK.
+    CONTROLLER_JOYSTICK_Y_SB = 24,
+    // C-Stick X Values
+    //
+    // Bits 32-39 tell the extent of the X value for the C-Stick
+    CONTROLLER_CSTICK_X_SB = 32,
+    // C-Stick Y Values
+    //
+    // Bits 40-47 tell C-Stick Y Value
+    CONTROLLER_CSTICK_Y_SB = 40,
+    // Left Trigger Press
+    //
+    // Bits 48-55 tell Left Trigger Press Value (how deeply it is pressed)
+    CONTROLLER_LEFT_TRIGGER_SB = 48,
+    // Right Trigger Press
+    //
+    // Bits 56-63 tell Right Trigger Press (how deeply it is pressed)
+    CONTROLLER_RIGHT_TRIGGER_SB = 56,
+} controller_button_t;
 
-//void send_start_word(void);
+void controller_init(unsigned int data_gpio);
 
-void controller_init(unsigned int out_gpio, unsigned int in_gpio);
+void controller_asm_write(void); 
 
-void controller_start_asm(void); 
+void controller_read(void);
 
 #endif
